eb90120135bcad2f618ca96292efb87178ff5825
ravi <ravi.20930@gmail.com>
2023-05-12 00:30:44 +0530

    boiler plate setup

---
+++ app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = require('http').createServer(app);
+app.use(bodyParser.json());
+app.use(express.json());
+exports.io = require('socket.io')(server);
+this.io.on('connection', socket => {
+    console.log('A client connected.');
+    socket.on('disconnect', () => {
+        console.log('A client disconnected.');
+    });
+    // Handle other events here
+});
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
+app.options(
+    "*",
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
+app.get("/", async (req, res) => {
+    res.json({
+        message: "hey",
+        status: 200
+    });
+});
+const n = server.listen(process.env.APP_PORT ? process.env.APP_PORT : 3000, () => {
+    console.log(`Backend Server is Live!\nListening on port: ${process.env.APP_PORT ? process.env.APP_PORT : 3000}`);
+});
